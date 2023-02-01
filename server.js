80262f53b03e1b784a62b72cd3b20ae6fbf66a31
Suvarna Jadhav <jadhav.suvarna28@gmail.com>
2023-02-01 15:57:05 -0500

    added delete functionality

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { addAbortSignal } = require('stream');
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -21,6 +21,10 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.post('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
@@ -32,30 +36,19 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    // Destructuring assignment for the items in req.body
     const { title, text } = req.body;
-
-    // If all the required properties are present
     if (title && text) {
-        // Variable for the object we will save
         const newNote = {
             title,
             text,
-            note_id: uuid(),
+            id: uuid(),
         };
-
-        // Obtain existing notes
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) {
                 console.error(err);
             } else {
-                // Convert string into JSON object
                 const parsedNotes = JSON.parse(data);
-
-                // Add a new review
                 parsedNotes.push(newNote);
-
-                // Write updated reviews back to the file
                 fs.writeFile(
                     './db/db.json',
                     JSON.stringify(parsedNotes, null, 4),
@@ -66,54 +59,38 @@ app.post('/api/notes', (req, res) => {
                 );
             }
         });
-
         const response = {
             status: 'success',
             body: newNote,
         };
-
-        // console.log(response);
         res.status(201).json(response);
     } else {
-        res.status(500).json('Error in posting review');
+        res.status(500).json('Error in posting note');
     }
 });
 
 // DELETE Route for a particular note
 app.delete('/api/notes/:id', (req, res) => {
-    // let params = req.params;
-    console.log(`${req.method} request received to add a review`);
-    const noteId  = req.params.id;
-    console.log("Note ID here : " + noteId);
+    let noteId = req.params.id;
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            let parsedNotes = JSON.parse(data);
+            parsedNotes = parsedNotes.filter((note) => note.id !== noteId);
+            fs.writeFile(
+                './db/db.json',
+                JSON.stringify(parsedNotes, null, 4),
+                (writeErr) =>
+                    writeErr
+                        ? console.error(writeErr)
+                        : console.info('Successfully updated notes!')
+            );
+            res.json()
+        }
+    });
 });
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
-
-
-// fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    //     if (err) {
-    //         console.error(err);
-    //     } else {
-    //         for (let x = 0; x < data.length; x++) { 
-    //             console.log("inside for loop" + data[x].note_id);
-    //             if (noteId === data[x].note_id) { 
-                    
-    //                 console.log("1 : " + noteId + "2 : " + data[x].note_id)
-    //             }
-    //         }
-    //         // const parsedNotesArray = JSON.parse(data);
-    //         // parsedNotesArray = parsedNotesArray.filter((note) => note.note_id !== noteId);
-    //         // console.log("Here" + newNoteArray);
-    //         // fs.writeFile(
-    //         //     './db/db.json',
-    //         //     JSON.stringify(newNoteArray, null, 4),
-    //         //     (writeErr) =>
-    //         //         writeErr
-    //         //             ? console.error(writeErr)
-    //         //             : console.info('Successfully updated notes!')
-    //         // );
-    //         // res.json(JSON.parse(newNoteArray))
-    //     }
-    // });
